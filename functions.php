c333ad38decb4ce8c1b2dab3a419e1b83396722d
Sk Baloch <itxskbaloch4@gmail.com>
2026-08-27 20:55:33 +0500

    Add functions.php to include necessary files

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+defined('ABSPATH') || exit;
+
+require_once get_template_directory() . '/inc/setup.php';
+require_once get_template_directory() . '/inc/enqueue.php';
+require_once get_template_directory() . '/inc/patterns.php';
+require_once get_template_directory() . '/inc/blocks.php';
+require_once get_template_directory() . '/inc/wpml.php';
